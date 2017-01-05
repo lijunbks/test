d0dee8e8c9d3e1abedd3f58e8bbdaa78d6488ef2
lijunbks <lij@ejiaofei.com>
2017-01-05 08:24:57 +0800

    test2

---
+++ maven/src/main/java/maven/test1.java
@@ -0,0 +1,24 @@
+package maven;
+
+public class test1 {
+	private String name;
+
+	public test1() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public test1(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
